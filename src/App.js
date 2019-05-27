17f9139a330f14b8ae24249c98c0f78c73acaa7b
quintarion <quintarion@gmail.com>
2019-05-27 17:45:27 +0200

    contact form

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div>
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
